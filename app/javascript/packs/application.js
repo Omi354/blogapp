ba138dfbd05519df68c7110bcbde73f17ec976aa
Omi <bell.tsaeb.shoji+work@gmail.com>
2024-09-06 13:15:42 +0900

    Error: unknown Statement of type "ForOfStatementが発生しました

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -19,9 +19,12 @@ require("trix");
 require("@rails/actiontext");
 
 import $ from "jquery";
+import axios from "axios";
 
 document.addEventListener("DOMContentLoaded", () => {
   $(".article_title").on("click", () => {
-    window.alert("CLICKED!");
+    axios.get("/").then((response) => {
+      console.log(response);
+    });
   });
 });
